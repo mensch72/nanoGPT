0667083215d6a555aeebed3143c95c7adba0d630
heitzig <heitzig@pik-potsdam.de>
2023-02-16 10:21:17 +0100

    slurm

---
+++ slurm/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --qos=gpushort
+#SBATCH --job-name=test_job
+#SBATCH --account=heitzig
+#SBATCH --output=test-%j.out
+#SBATCH --error=test-%j.err
+#SBATCH --workdir=/p/tmp/heitzig
+
+hostname
