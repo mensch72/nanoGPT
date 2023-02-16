a4200887f74dff2bec0e31fb5bab03c173bf8fb3
heitzig <heitzig@pik-potsdam.de>
2023-02-16 16:17:40 +0100

    faster

--- slurm/test.sh
+++ slurm/test.sh
@@ -11,13 +11,14 @@
 
 source activate torch
 cd /home/heitzig/gane/rl/git/nanoGPT
+module load compiler/gnu/7.3.0
 module load intel/2018.3
 #module load mpi/intel/5.1.3
 export CC=icc
 module list
 which icc
 python data/shakespeare_char/prepare.py
-time python -u train.py config/train_shakespeare_char_small_ternary2.py --device=cuda --compile=False --max_iters=3000 --lr_decay_iters=3000 --out_dir=/p/tmp/heitzig/out-shakespeare-char-ternary2
-time python -u train.py config/train_shakespeare_char_small.py --device=cuda --compile=False --max_iters=3000 --lr_decay_iters=3000 --out_dir=/p/tmp/heitzig/out-shakespeare-char
-time python sample.py --out_dir=/p/tmp/heitzig/out-shakespeare-char-ternary2 --device=cuda --compile=False
-time python sample.py --out_dir=/p/tmp/heitzig/out-shakespeare-char --device=cuda --compile=False
+time python -u train.py config/train_shakespeare_char_small_ternary2.py --device=cuda --compile=True --max_iters=3000 --lr_decay_iters=3000 --out_dir=/p/tmp/heitzig/out-shakespeare-char-ternary2
+time python -u train.py config/train_shakespeare_char_small.py --device=cuda --compile=True --max_iters=3000 --lr_decay_iters=3000 --out_dir=/p/tmp/heitzig/out-shakespeare-char
+time python sample.py --out_dir=/p/tmp/heitzig/out-shakespeare-char-ternary2 --device=cuda --compile=True
+time python sample.py --out_dir=/p/tmp/heitzig/out-shakespeare-char --device=cuda --compile=True
